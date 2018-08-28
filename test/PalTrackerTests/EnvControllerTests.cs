aa74ca087c7ee14f13d78f6fb74814be9a44e5da
Deepak <deepakpesit@hotmail.com>
2018-08-28 14:11:33 +0530

    Third commit

---
+++ test/PalTrackerTests/EnvControllerTests.cs
@@ -0,0 +1,18 @@
+﻿using PalTracker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PalTrackerTests
+{
+    public class EnvControllerTests
+    {
+        [Fact]
+        public void GetTest()
+        {
+            CloudFoundryInfo info = new CloudFoundryInfo();            
+            //EnvController controller = new EnvController();
+        }
+    }
+}
---
+++ test/PalTrackerTests/WelcomeControllerTests.cs
@@ -0,0 +1,23 @@
+﻿using PalTracker;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace PalTrackerTests
+{
+    public class WelcomeControllerTests
+    {
+        [Fact]
+        public void GetTest()
+        {
+            WelcomeMessage message = new WelcomeMessage("Testing");
+            WelcomeController controller = new WelcomeController(message);
+            string result = controller.SayHello();
+            Assert.NotEmpty(result);
+            Assert.NotNull(result);
+            Assert.True(result.Length > 1);
+            
+        }
+    }
+}
